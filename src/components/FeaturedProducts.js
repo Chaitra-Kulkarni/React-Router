a67e92c01af3c954532b01f6351860d304f88b02
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-17 18:00:37 +0530

    minor: index Route

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,7 +1,17 @@
 export const FeaturedProducts = () => {
-    return (
-        <div>
-            <h2>Featured Products!!</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Featured Products!!</h2>
+      <div className="orders-wrapper">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+    </div>
+  );
+};
